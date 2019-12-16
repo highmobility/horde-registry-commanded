e6f61083ee4197a23fc3ef6b59dc86f78455b26b
Milad Rastian <milad@high-mobility.com>
2019-12-16 16:51:50 +0100

    Add Tests using Erlang :slave module

---
+++ lib/integration/cluster.ex
@@ -0,0 +1,46 @@
+defmodule Integration.Cluster do
+  @moduledoc false
+
+  def stop(node) do
+    :slave.stop(node)
+  end
+
+  def prepare do
+    # Turn node into a distributed node with the given long name
+    :net_kernel.start([:"primary@127.0.0.1"])
+
+    # Allow spawned nodes to fetch all code from this node
+    :erl_boot_server.start([])
+    {:ok, ipv4} = :inet.parse_ipv4_address('127.0.0.1')
+    :erl_boot_server.add_slave(ipv4)
+  end
+
+  def spawn(node) do
+    # Start Node
+    {:ok, node} =
+      :slave.start(
+        '127.0.0.1',
+        node,
+        '-loader inet -hosts 127.0.0.1 -setcookie #{:erlang.get_cookie()}'
+      )
+
+    :rpc.block_call(node, :code, :add_paths, [:code.get_path()])
+
+    # Start Mix
+    :rpc.block_call(node, Application, :ensure_all_started, [:mix])
+    :rpc.block_call(node, Mix, :env, [:dev])
+
+    # Load Personalized Config
+    for {key, val} <-
+          Keyword.get(:rpc.block_call(node, Config.Reader, :read!, ["config.exs"]), :integration) do
+      :rpc.block_call(node, Application, :put_env, [:integration, key, val])
+    end
+
+    # Start Apps
+    for {app_name, _, _} <- Application.loaded_applications() do
+      :rpc.block_call(node, Application, :ensure_all_started, [app_name])
+    end
+
+    {:ok, node}
+  end
+end
---
+++ test/slave_test.exs
@@ -0,0 +1,220 @@
+defmodule SlaveTest do
+  use ExUnit.Case, async: false
+
+  def clear_db do
+    config = Integration.EventStore.config()
+    :ok = EventStore.Tasks.Drop.exec(config, [])
+    :ok = EventStore.Tasks.Create.exec(config, [])
+    :ok = EventStore.Tasks.Init.exec(Integration.EventStore, config, [])
+  end
+
+  def rpc(node, module, function, arguments) do
+    ##
+    # TODO: Pass PID to command/event so test can know when it was processed
+    ##
+    a = :rpc.block_call(node, module, function, arguments)
+    Process.sleep(100)
+    a
+  end
+
+  setup_all do
+    clear_db()
+
+    Integration.Cluster.prepare()
+  end
+
+  test "start and connect 2 nodes" do
+    {:ok, node_a} = Integration.Cluster.spawn(:node_a)
+    {:ok, node_b} = Integration.Cluster.spawn(:node_b)
+
+    assert [:"primary@127.0.0.1", :"node_b@127.0.0.1"] = :rpc.block_call(node_a, Node, :list, [])
+    assert [:"primary@127.0.0.1", :"node_a@127.0.0.1"] = :rpc.block_call(node_b, Node, :list, [])
+
+    Integration.Cluster.stop(node_a)
+    Integration.Cluster.stop(node_b)
+  end
+
+  test "Run Commands in a single node" do
+    # - Start NodeA
+    {:ok, node_a} = Integration.Cluster.spawn(:node_a)
+
+    uuid = UUID.uuid4()
+
+    # - Run Command Start
+    assert :ok =
+             rpc(node_a, Integration.App, :dispatch, [
+               %Integration.Commands.Create{uuid: uuid, message: "create"}
+             ])
+
+    # - Inspect State Aggregate
+    assert %Integration.Aggregate{message: "create", uuid: uuid} =
+             rpc(node_a, Commanded.Aggregates.Aggregate, :aggregate_state, [
+               Integration.App,
+               Integration.Aggregate,
+               uuid
+             ])
+
+    # - Inspect State EventHandler
+    assert %Integration.EventHandler{
+             message: "create",
+             uuid: uuid
+           } = rpc(node_a, Integration.EventHandler, :state, [])
+
+    Integration.Cluster.stop(node_a)
+  end
+
+  test "Run Commands in 2 different Nodes" do
+    # - Start Nodes
+    {:ok, node_a} = Integration.Cluster.spawn(:node_a)
+    {:ok, node_b} = Integration.Cluster.spawn(:node_b)
+
+    uuid = UUID.uuid4()
+
+    # - Run Command Create in NodeA
+    assert :ok =
+             rpc(node_a, Integration.App, :dispatch, [
+               %Integration.Commands.Create{uuid: uuid, message: "create"}
+             ])
+
+    # - Run Command Update in NodeB
+    assert :ok =
+             rpc(node_b, Integration.App, :dispatch, [
+               %Integration.Commands.Update{uuid: uuid, message: "update"}
+             ])
+
+    # - Inspect State Aggregate in Node A
+    assert %Integration.Aggregate{message: "update", uuid: uuid, node: :"node_a@127.0.0.1"} =
+             rpc(node_a, Commanded.Aggregates.Aggregate, :aggregate_state, [
+               Integration.App,
+               Integration.Aggregate,
+               uuid
+             ])
+
+    # - Inspect State Aggregate in Node B
+    assert %Integration.Aggregate{message: "update", uuid: uuid, node: :"node_a@127.0.0.1"} =
+             rpc(node_b, Commanded.Aggregates.Aggregate, :aggregate_state, [
+               Integration.App,
+               Integration.Aggregate,
+               uuid
+             ])
+
+    # - Inspect where EventHandler is running
+    assert :"node_a@127.0.0.1" = rpc(node_a, Integration.EventHandler, :where_am_i, [])
+
+    # - Inspect State EventHandler in Node A
+    assert %Integration.EventHandler{
+             message: "update",
+             uuid: uuid
+           } = rpc(node_a, Integration.EventHandler, :state, [])
+
+    # - Inspect Event Handler Node B
+    refute rpc(node_b, Process, :whereis, [Integration.EventHandler])
+
+    Integration.Cluster.stop(node_a)
+    Integration.Cluster.stop(node_b)
+  end
+
+  test "Check State when a Node is Killed" do
+    # - Start Nodes
+    {:ok, node_a} = Integration.Cluster.spawn(:node_a)
+    {:ok, node_b} = Integration.Cluster.spawn(:node_b)
+
+    uuid = UUID.uuid4()
+
+    # - Run Command Start in NodeA
+    assert :ok =
+             rpc(node_a, Integration.App, :dispatch, [
+               %Integration.Commands.Create{uuid: uuid, message: "create"}
+             ])
+
+    # - Kill NodeA
+    Integration.Cluster.stop(node_a)
+
+    # - Run Command Update in NodeB
+    assert :ok =
+             rpc(node_b, Integration.App, :dispatch, [
+               %Integration.Commands.Update{uuid: uuid, message: "update"}
+             ])
+
+    # - Inspect State Aggregate in Node B
+    assert %Integration.Aggregate{message: "update", uuid: uuid, node: :"node_b@127.0.0.1"} =
+             rpc(node_b, Commanded.Aggregates.Aggregate, :aggregate_state, [
+               Integration.App,
+               Integration.Aggregate,
+               uuid
+             ])
+
+    # - Inspect Event Handler Node B
+    assert :"node_b@127.0.0.1" = rpc(node_b, Integration.EventHandler, :where_am_i, [])
+
+    # - Inspect State EventHandler in Node B
+    assert %Integration.EventHandler{
+             message: "update",
+             uuid: uuid
+           } = rpc(node_b, Integration.EventHandler, :state, [])
+
+    Integration.Cluster.stop(node_b)
+  end
+
+  test "When a node is killed but join again later" do
+    # - Start Nodes
+    {:ok, node_a} = Integration.Cluster.spawn(:node_a)
+    {:ok, node_b} = Integration.Cluster.spawn(:node_b)
+
+    uuid = UUID.uuid4()
+
+    # - Run Command Start in NodeA
+    assert :ok =
+             rpc(node_a, Integration.App, :dispatch, [
+               %Integration.Commands.Create{uuid: uuid, message: "create"}
+             ])
+
+    # - Kill NodeA
+    Integration.Cluster.stop(node_a)
+
+    # - Run Command Update in NodeB
+    assert :ok =
+             rpc(node_b, Integration.App, :dispatch, [
+               %Integration.Commands.Update{uuid: uuid, message: "update"}
+             ])
+
+    # - Start Node A
+    {:ok, node_a} = Integration.Cluster.spawn(:node_a)
+
+    # - Run Command Delete in NodeA
+    assert :ok =
+             rpc(node_a, Integration.App, :dispatch, [
+               %Integration.Commands.Delete{uuid: uuid, message: "delete"}
+             ])
+
+    # - Inspect State Aggregate in Node B
+    assert %Integration.Aggregate{message: "delete", uuid: uuid, node: :"node_b@127.0.0.1"} =
+             rpc(node_b, Commanded.Aggregates.Aggregate, :aggregate_state, [
+               Integration.App,
+               Integration.Aggregate,
+               uuid
+             ])
+
+    # - Inspect State Aggregate in Node A
+    assert %Integration.Aggregate{message: "delete", uuid: uuid, node: :"node_b@127.0.0.1"} =
+             rpc(node_a, Commanded.Aggregates.Aggregate, :aggregate_state, [
+               Integration.App,
+               Integration.Aggregate,
+               uuid
+             ])
+
+    node_a_result =
+      rpc(node_a, Process, :whereis, [Integration.EventHandler]) &&
+        rpc(node_a, Integration.EventHandler, :state, [])
+
+    node_b_result =
+      rpc(node_b, Process, :whereis, [Integration.EventHandler]) &&
+        rpc(node_b, Integration.EventHandler, :state, [])
+
+    assert %Integration.EventHandler{message: "delete", uuid: uuid} =
+             Enum.find([node_a_result, node_b_result], &is_map/1)
+
+    Integration.Cluster.stop(node_a)
+    Integration.Cluster.stop(node_b)
+  end
+end
