14b5d7933639978f46aa3e0fc91c1a2f52074984
Francesco Donadon <francesco@high-mobility.com>
2020-06-08 17:21:12 +0200

    Tentative upgrade to commanded_eventstore_adapter 1.1

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule HordeRegistryCommanded.MixProject do
       {:horde, "~> 0.7.1"},
       {:commanded, "~> 1.1"},
       {:jason, "~> 1.1", only: [:dev, :test]},
-      {:commanded_eventstore_adapter, "~> 1.0.0", only: [:dev, :test]},
+      {:commanded_eventstore_adapter, "~> 1.1", only: [:dev, :test]},
       {:eventstore,
        git: "https://github.com/scudelletti/eventstore.git",
        branch: "ds-fix-distributed",
